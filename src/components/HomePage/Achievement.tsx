43634d9e268b6f8087824a93fc789e10b36bcd70
Osmond Matsela <osmondmatsela@gmail.com>
2025-11-30 01:26:28 +0200

    added archievements table

--- src/components/HomePage/Achievement.tsx
+++ src/components/HomePage/Achievement.tsx
@@ -1,21 +1,49 @@
 import { Trophy } from 'lucide-react';
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 const achievements = [
     {
-      year: "2024",
-      title: "Provincial Mathematics Olympiad - 1st Place",
-      description: "Our Grade 11 team secured first place in the Limpopo Mathematics Olympiad"
+      year: "2021",
+      bachelors: "119",
+      diplomas: "50",
+      higherCertificate:"13",
+      distinctions:"82",
+      bachelorsPer: "65,5",
+      diplomasPer: "27,5",
+      higherCertificatePer:"7,1",
+      distinctionsPer:"-"
     },
     {
-      year: "2023", 
-      title: "Best Performing Rural School Award",
-      description: "Recognized by the Department of Education for outstanding academic performance"
+      year: "2022",
+      bachelors: "103",
+      diplomas: "35",
+      higherCertificate:"5",
+      distinctions:"135",
+      bachelorsPer: "72",
+      diplomasPer: "24,6",
+      higherCertificatePer:"3,5",
+      distinctionsPer:"-"
     },
     {
       year: "2023",
-      title: "Inter-School Debate Championship",
-      description: "Won the regional debate championship for the third consecutive year"
+      bachelorsPer: "70,35",
+      diplomasPer: "20.93",
+      higherCertificatePer:"8.72",
+      distinctions:"145",
+      bachelors: "121",
+      diplomas: "36",
+      higherCertificate:"15",
+      distinctionsPer:"-",
+    },
+    {
+      year: "2024",
+      bachelorsPer: "70,35",
+      diplomasPer: "20.93",
+      higherCertificatePer:"8.72",
+      distinctions:"145",
+      bachelors: "121",
+      diplomas: "36",
+      higherCertificate:"15",
+      distinctionsPer:"-",
     }
   ];
 const Achievement = () => {
@@ -26,22 +54,50 @@ const Achievement = () => {
               <Trophy className="w-8 h-8 mr-3 text-red-800" />
               Our Achievements
             </h2>
-            <p className="text-gray-600 mt-2">Celebrating excellence in academics and extracurricular activities</p>
+            <p className="text-gray-600 mt-2">Celebrating excellence in academics</p>
           </div>
           
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
             {achievements.map((achievement, index) => (
-              <Card key={index} className="hover:shadow-lg transition-shadow">
-                <CardHeader>
-                  <div className="flex items-center justify-between">
-                    <CardTitle className="text-lg">{achievement.title}</CardTitle>
-                    <span className="text-2xl font-bold text-red-800">{achievement.year}</span>
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <p className="text-gray-600">{achievement.description}</p>
-                </CardContent>
-              </Card>
+              <table key={index} className="hover:shadow-lg transition-shadow rounded bg-red-800">
+                <caption className="text-2xl font-bold text-gray-900 p-3">
+                  Matric results class of {achievement.year}
+                </caption>
+                <thead className="">
+                  <tr className = "text-white ">
+                    <th className='p-4'>Description</th>
+                    <th></th>
+                    <th>Percentage</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="text-white pb-4">
+                    <th className='p-4'></th>
+                    <th>No</th>
+                    <th>%</th>  
+                  </tr>
+                  <tr className="ligt-custom-pink pb-4">
+                    <th className='p-4'>Bachelors</th>
+                    <th>{achievement.bachelors}</th>
+                    <th>{achievement.bachelorsPer}</th>  
+                  </tr>
+                  <tr className="dark-custom-pink pb-4">
+                    <th className='p-4'>Diplomas</th>
+                    <th>{achievement.diplomas}</th>
+                    <th>{achievement.diplomasPer}</th>  
+                  </tr>
+                  <tr className="ligt-custom-pink pb-4">
+                    <th className='p-4'>Hight Certificate</th>
+                    <th>{achievement.higherCertificate}</th>
+                    <th>{achievement.higherCertificatePer}</th>  
+                  </tr>
+                  <tr className="dark-custom-pink pb-4">
+                    <th className='p-4'>Distinctions</th>
+                    <th>{achievement.distinctions}</th>
+                    <th >{achievement.distinctionsPer}</th>  
+                  </tr>
+                </tbody>
+              </table>
             ))}
           </div>
         </>
--- src/components/HomePage/PlatformAccess.tsx
+++ src/components/HomePage/PlatformAccess.tsx
@@ -14,7 +14,7 @@ const PlatformAccess = () => {
         {
       showCalendar ? <CalendarComponent close={setShowCalendar}/> : null
     }
-             <div className="text-center">
+          <div className="text-center">
             <Lock className="w-12 h-12 mx-auto text-red-800 mb-4" />
             <h2 className="text-3xl font-bold text-gray-900 mb-4">Digital Platform Access</h2>
             <p className="text-gray-700 text-lg mb-6">
